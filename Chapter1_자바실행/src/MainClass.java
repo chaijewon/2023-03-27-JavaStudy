f6a0fdeade22688bca328a4d32d574f6d749b79f
SIST <SIST@DESKTOP-08E10UE>
2023-03-28 16:02:25 +0900

    1111

---
+++ Chapter1_자바실행/src/MainClass.java
@@ -0,0 +1,78 @@
+// 한줄 주석 
+/*
+ *   여러줄 주석 ==> ctrl+shift+/  
+ *   = 기본 문법 사항 (19  page)
+ *     1. 파일명과 클래스명은 동일하다 
+ *        class A  => A.java
+ *     2. 클래스안에는 반드시 main()을 포함해야된다 
+ *                       -------실행시에 시작점 
+ *     3. 클래스명은 처음자를 대문자로 시작 (문법이 아니라 약속)
+ *        클래스명은 한글도 가능하다 
+ *     4. 자바의 포현법 
+ *        문자열  ==> 문자여러개사용 ==> "Hello"
+ *        문자   ==> 한개의 글자 ==> 'H','홍' ==> 'Hello' (오류)
+ *        숫자   
+ *          ==> 정수/실수 
+ *              = 10진법  100,60... 
+ *              = 8진법   012 => 10
+ *                       - 
+ *              = 16진법  0x13
+ *              *****= 2진법   0b000111
+ *              = 10.11 , 10.11F
+ *                double  float 
+ *                 8byte   4byte
+ *        논리   ==> true/false 
+ *        특수문자 
+ *           \t  tab (일정 간격)
+ *           ******\n  new Line (다음줄에 출력)
+ *           \"
+ *           ****** \\
+ *           
+ *       메소드 : 기능 수행 : 자바 
+ *       class 안에 있는 지 여부 
+ *       함수 : C/Python 
+ *       =====================
+ *       
+ *       1. 표현법 
+ *          정수/실수 
+ *          10진법 10,20....
+ *          8진법 0XXX(0~7)
+ *          16진법 0xXXX(0~9) A~F
+ *                0~15  10(A)  0xA 0xFF
+ *          2진법  0bXXXX(0~1)
+ *       2. 문자 
+ *          문자 여러개 "" / 한개 ''
+ *       3. 논리 
+ *          true/false 
+ *       ----------------------------------
+ *       4. 주석 
+ *          한줄  //
+ *          여러줄 /*
+ *          
+ *       5. 실행 과정 
+ *       A.java  =========>  컴퓨터가 인식하는 언어로 변경 =====>  실행
+ *       ------                컴파일  (javac) => A.class   인터프리터 java
+ *       원시소스 (프로그래머만 아는 언어)
+ *       
+ *       => 컴파일 :  C/C++
+ *       => 인터프리터 : HTML/XML..Java
+ *       
+ *       
+ */
+public class MainClass {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		
+		/*
+		 * System.out.println("Hello\tJava"); 
+		 * System.out.println("Hello\n");
+		 * System.out.println("Java"); System.out.println("\"홍길동\""); //"홍길동"
+		 * System.out.println("C:\\javaDev"); System.out.println(10);
+		 * System.out.println('A'); System.out.println(true);
+		 */
+		 
+	}
+
+}
---
+++ Chapter1_자바실행/src/MainClass2.java
@@ -0,0 +1,43 @@
+
+public class MainClass2 {
+   public static void main(String[] args) {
+	  // 프로그램의 시작점 / 끝점 
+	  /*
+	   *    정수 
+	   *    ---
+	   *    2진법 0b
+	   *    8진법  0
+	   *    16진법  0x
+	   *    
+	   *    29 => 2진법 
+	   *    64 32 16 8 4 2 0(1)
+	   *           1 1 1 0  1    => 11101
+	   *    76 => 2진법 
+	   *    1   0  0  1 1 0 0
+	   */
+	   System.out.println(0b11101);
+	   System.out.println(0b1001100);
+	   System.out.println(Integer.toBinaryString(29));
+	   // 11101
+	   /*
+	    *   11101
+	    *     ---
+	    *  ---  5
+	    *    3  -------------> 035
+	    *    
+	    *   11101
+	    *    ----
+	    *  --  13 => 10-a,11-b,12-c,13-d
+	    *   1 ==> 1d
+	    */
+	   System.out.println(Integer.toOctalString(29));
+	   System.out.println(Integer.toHexString(29));
+	   
+	   System.out.println("c:\\dev");
+	   System.out.print("Hello\n");
+	   System.out.println("Hello");
+	   //System.out.printf("%d+%d=%d",10,20,30);
+	   // 10+20=30
+	   System.out.println(10+"+"+20+"="+30);
+   }
+}
